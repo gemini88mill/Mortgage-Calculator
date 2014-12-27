ab5ac19c5c6a06c1131b28d449f492b642a6cbdb
Raphael <gemini88mill@gmail.com>
2014-12-27 09:43:01 -0500

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public class Main {
+
+    /**
+     * Mortgage Calculator
+     * @author Raphael Miller (gemini88mill)
+     *
+     * Program designed to give data on how long it will take to to
+     * pay off your mortgage. As if you didnt already feel the cold
+     * uncomprimising hands of debt clutching you in a meloncholy
+     * embrace :)
+     *
+     * Mortgage Calculator – Calculate the monthly payments of a
+     * fixed term mortgage over given Nth terms at a given interest rate.
+     * Also figure out how long it will take the user to pay back the loan.
+     *
+     * @param args
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("hello world");
+    }
+}
